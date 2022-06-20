c7e4d1914a45c6099399654960df24115d074957
SQflex <egorkazgorki2@gmail.com>
2022-06-20 12:15:43 +0300

    fix: warnings

--- src/components/EditNote.js
+++ src/components/EditNote.js
@@ -1,4 +1,4 @@
-import { useState, useEffect} from 'react';
+// import { useState, useEffect} from 'react';
 //import ContentEditable from 'react-contenteditable';
 
 const EditNote = ({editFormData, handleEditFormChange, handleEditFormSubmit}) => {
